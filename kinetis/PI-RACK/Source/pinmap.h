4b1684740f4adf021051d43e4e7fd92e2ca7eaf8
Nicolás <nicolas.venturo@gmail.com>
2015-06-15 00:30:57 -0300

    Defined and tested the final pinmap!

--- kinetis/PI-RACK/Source/pinmap.h
+++ kinetis/PI-RACK/Source/pinmap.h
@@ -2,14 +2,33 @@
 #define PINMAP_H_
 
 #include "MKE02Z2.h"
+#include "utils.h"
 
-#define PEDAL
+// Inputs
+#define PEDAL GPIO_PTD0
 
-#define BUTTON_LEFT
-#define BUTTON_RIGHT
+#define BUTTON_LEFT GPIO_PTB2
+#define BUTTON_RIGHT GPIO_PTA0
 
-#define POWER_LED
-#define STATUS_LED
+// Three position slider
+#define SLIDER_1_A GPIO_PTE5
+#define SLIDER_1_B GPIO_PTE6
+
+// Two position slider
+#define SLIDER_2_A GPIO_PTC5
+
+// Four position slider
+#define SLIDER_3_A GPIO_PTE4
+#define SLIDER_3_B GPIO_PTB4
+#define SLIDER_3_C GPIO_PTD1
+
+// Outputs
+#define POWER_LED GPIO_PTB0
+#define STATUS_LED GPIO_PTD5
+
+// The LEDs are common anode (they must be brought low to be turned on)
+#define LED_ON(led) (PIN_OFF(led))
+#define LED_OFF(led) (PIN_ON(led))
 
 #define SLIDER_1_ADC_CH 8
 #define SLIDER_2_ADC_CH 9
